4f81f02bcc58bd9a79a055f2cc790b870745fa78
Gavin97Dmello <gavin@zaya.in>
2020-09-10 13:49:08 +0530

    Initial Commit

---
+++ besomeone-native/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  besomeone-native
+//
+//  Created by Zaya Developer on 10/09/20.
+//  Copyright © 2020 Zaya Developer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
